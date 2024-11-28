617511f104696cbfd0fcba370d3f4db8f2c1d712
ShadowValeKnight <104908175+ShadowValeKnight@users.noreply.github.com>
2024-11-28 00:15:33 -0600

    Update run_baseline_parallel_fast.py

--- baselines/run_baseline_parallel_fast.py
+++ baselines/run_baseline_parallel_fast.py
@@ -2,7 +2,7 @@ from os.path import exists
 from pathlib import Path
 import uuid
 from red_gym_env import RedGymEnv
-from stable_baselines3 import A2C, PPO, DDPG
+from stable_baselines3 import A2C, PPO
 from stable_baselines3.common import env_checker
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 from stable_baselines3.common.utils import set_random_seed
@@ -38,7 +38,8 @@ if __name__ == '__main__':
         'print_rewards': True, 'save_video': True, 'fast_video': True, 'session_path': sess_path,
         'gb_path': '../PokemonRed.gb', 'debug': False, 'sim_frame_dist': 2_000_000.0,
         'use_screen_explore': True, 'reward_scale': 4, 'extra_buttons': False,
-        'explore_weight': 3  # 2.5
+        'explore_weight': 3,  # 2.5
+        'curriculum_stage': 1  # Start with stage 1
     }
 
     print(env_config)
@@ -49,21 +50,27 @@ if __name__ == '__main__':
     checkpoint_callback = CheckpointCallback(save_freq=ep_length, save_path=sess_path,
                                              name_prefix='poke')
     # env_checker.check_env(env)
-    learn_steps = 40
+    learn_steps = 15
     # put a checkpoint here you want to start from
     file_name = 'session_e41c9eff/poke_38207488_steps'
 
     if exists(file_name + '.zip'):
         print('\nloading checkpoint')
-        model = DDPG.load(file_name, env=env)
+        model = PPO.load(file_name, env=env)
         model.n_steps = ep_length
         model.n_envs = num_cpu
         model.rollout_buffer.buffer_size = ep_length
         model.rollout_buffer.n_envs = num_cpu
         model.rollout_buffer.reset()
     else:
-        model = DDPG('CnnPolicy', env, verbose=1, gamma=0.998, batch_size=128, steps=ep_length // 8)
+        model = PPO('CnnPolicy', env, verbose=1, gamma=0.998, batch_size=128, steps=ep_length // 8)
 
-    for i in range(learn_steps):
-        model.learn(total_timesteps=(ep_length) * num_cpu * 1000, callback=checkpoint_callback)
+    curriculum_stages = 4
 
+    for stage in range(1, curriculum_stages + 1):
+        print(f"\nStarting curriculum stage {stage}\n")
+        env_config['curriculum_stage'] = stage
+        env = SubprocVecEnv([make_env(i, env_config) for i in range(num_cpu)])
+        model.set_env(env)
+        for i in range(learn_steps):
+            model.learn(total_timesteps=(ep_length) * num_cpu * 1000, callback=checkpoint_callback)
